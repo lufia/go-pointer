026ea1308229d029c22b549ef5dc07dae46b55eb
lufia <lufia@lufia.org>
2022-03-20 15:09:14 +0900

    add generate.go for basic types

---
+++ generate.go
@@ -0,0 +1,88 @@
+//go:build ignore
+
+package main
+
+import (
+	"bytes"
+	"embed"
+	"flag"
+	"fmt"
+	"go/format"
+	"log"
+	"os"
+	"path/filepath"
+	"text/template"
+	"unicode"
+)
+
+var (
+	flagType = flag.String("type", "", "`name` of the builtin type")
+)
+
+func main() {
+	log.SetFlags(0)
+	flag.Usage = func() {
+		name := filepath.Base(os.Args[0])
+		w := flag.CommandLine.Output()
+		fmt.Fprintf(w, "usage: %s [options] [data ...]\n\n", name)
+		flag.PrintDefaults()
+		os.Exit(2)
+	}
+	flag.Parse()
+
+	if *flagType == "" {
+		flag.Usage()
+	}
+	err := generate(&Target{
+		Type:     *flagType,
+		FuncName: title(*flagType),
+		TestData: flag.Args(),
+	})
+	if err != nil {
+		log.Fatalf("cannot generate: %v", err)
+	}
+}
+
+func title(s string) string {
+	if s == "" {
+		return ""
+	}
+	u := []rune(s)
+	return string(unicode.ToUpper(u[0])) + string(u[1:])
+}
+
+//go:embed *.tmpl
+var content embed.FS
+
+var (
+	funcTmpl = template.Must(template.ParseFS(content, "func.tmpl"))
+	testTmpl = template.Must(template.ParseFS(content, "test.tmpl"))
+)
+
+type Target struct {
+	Type     string
+	FuncName string
+	TestData []string
+}
+
+func generate(t *Target) error {
+	if err := execute(t.Type+".go", funcTmpl, t); err != nil {
+		return err
+	}
+	if err := execute(t.Type+"_test.go", testTmpl, t); err != nil {
+		return err
+	}
+	return nil
+}
+
+func execute(name string, tmpl *template.Template, t *Target) error {
+	var w bytes.Buffer
+	if err := tmpl.Execute(&w, t); err != nil {
+		return err
+	}
+	data, err := format.Source(w.Bytes())
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(name, data, 0664)
+}
